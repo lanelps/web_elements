ff0fd84be5df39925da0839728b967853919b49d
Lane le Prevost-Smith <lanelps@gmail.com>
2019-03-12 17:57:56 +1300

    css and style loader added

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import App from './components/App'
-import 'styles.css'
+import './styles.css'
 
 render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
