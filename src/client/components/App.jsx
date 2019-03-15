d167f937f03e1a195d5786e5de79ee3c5a2fb0ea
Lane le Prevost-Smith <lanelps@gmail.com>
2019-03-15 18:04:17 +1300

    added new test

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Test1 from './Test1'
 import Test2 from './Test2'
 import Test3 from './Test3'
+import Test3 from './Test4'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Test1 />
       <Test2 />
       <Test3 />
+      <Test4 />
     </React.Fragment>
   )
 }
