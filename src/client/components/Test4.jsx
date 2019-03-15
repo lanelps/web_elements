d6fe0ac2cbaf087f376672720ffbd34ee0c98042
Lane le Prevost-Smith <lanelps@gmail.com>
2019-03-15 18:01:46 +1300

    added new test

---
+++ src/client/components/Test4.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Test4() {
+  return (
+    <React.Fragment>
+      <div className="test4">
+        <div />
+        <div />
+        <div />
+        <div />
+      </div>
+    </React.Fragment>
+  )
+}
