7b02b235ee6d817c74e1260594749b19cb8ae282
Lane le Prevost-Smith <lanelps@gmail.com>
2019-03-22 17:50:10 +1300

    Added Element 7 which Rotates 45 deg

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -6,6 +6,7 @@ import Element3 from './Element3'
 import Element4 from './Element4'
 import Element5 from './Element5'
 import Element6 from './Element6'
+import Element7 from './Element7'
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
       <Element4 />
       <Element5 />
       <Element6 />
+      <Element7 />
     </React.Fragment>
   )
 }
---
+++ src/client/components/Element7.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Element7() {
+  return (
+    <React.Fragment>
+      <div className="test7">
+        <div className="seven"/>
+      </div>
+    </React.Fragment>
+  )
+}
